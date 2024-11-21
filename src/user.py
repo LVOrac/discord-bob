697ba15cda8bd40491b1628c92fe5a551d65c919
LVOrac <joecheong2006@gmail.com>
2024-11-21 15:20:11 -0800

    move init

--- src/user.py
+++ src/user.py
@@ -1,9 +1,10 @@
 import os
+import shutil
 from discord import Interaction
 
 def initialized_user(interaction: Interaction) -> str | None:
     user_folder = str(interaction.user.id)
-    if os.path.exists(user_folder) and os.path.isdir(user_folder):
+    if os.path.exists(user_folder):
         return user_folder
     return None
 
@@ -11,3 +12,18 @@ def init_user(interaction: Interaction) -> None:
     user_folder = str(interaction.user.id)
     os.makedirs(user_folder, exist_ok=True)
 
+async def init(interaction: Interaction) -> None:
+    path = initialized_user(interaction)
+    if path == None:
+        init_user(interaction)
+        await interaction.response.send_message(str(interaction.user.id) + ": init complete!")
+        return
+    await interaction.response.send_message(str(interaction.user.id) + ": already exists!")
+
+async def delete(interaction: Interaction) -> None:
+    path = initialized_user(interaction)
+    if path == None:
+        await interaction.response.send_message(str(interaction.user.id) + ": user do not exist!")
+        return
+    shutil.rmtree(path)
+    await interaction.response.send_message(str(interaction.user.id) + ": delete complete!")
