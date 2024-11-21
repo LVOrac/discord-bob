47edd744a9fc081bb9f2e6cdae6080486858e72c
LVOrac <joecheong2006@gmail.com>
2024-11-21 15:21:15 -0800

    remove init

--- src/main.py
+++ src/main.py
@@ -17,29 +17,15 @@ folder_path: str = os.path.join("users")
 os.chdir(folder_path)
 
 from bot_setup import client, TOKEN, tree
-from discord import Message, Interaction
-import text_style as ts
+from discord import Message
+from discord.app_commands import Command
 
 from todo_commands import TodoCommands
+from user import init, delete
+
 tree.add_command(TodoCommands())
-
-from user import initialized_user, init_user
-
-@tree.command(name="fontstyles", description="this will show some font styles")
-async def fontstyles(interaction: Interaction) -> None:
-    out = ""
-    for style in range(0b111111):
-        out += ts.format("This is some text\u2665", style=style) + '\n'
-    await interaction.response.send_message(out)
-
-@tree.command(name="init", description="this will generate an init folder")
-async def init(interaction: Interaction) -> None:
-    path = initialized_user(interaction)
-    if not path:
-        init_user(interaction)
-        await interaction.response.send_message(str(interaction.user.id) + ": init complete!")
-        return
-    await interaction.response.send_message(str(interaction.user.id) + ": already exists!")
+tree.add_command(Command(name="init", description="initialize user", callback=init))
+tree.add_command(Command(name="del", description="delete user", callback=delete))
 
 @client.event
 async def on_ready() -> None:
