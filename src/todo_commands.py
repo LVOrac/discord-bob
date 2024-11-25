f38b5954fea1607014caf5e5218526d3d3af1353
LVOrac <joecheong2006@gmail.com>
2024-11-25 00:09:55 -0800

    add update_lifetime

--- src/todo_commands.py
+++ src/todo_commands.py
@@ -4,26 +4,9 @@ from discord import Interaction
 from typing import Optional
 from discord.app_commands import Group, Choice, command, describe, choices
 from text_style import Style, format
-
-def read_todo(id: int):
-    if not os.path.exists(str(id)):
-        return None
-    todo_path: str = os.path.join(str(id), "todolist.json")
-    if not os.path.exists(todo_path):
-        with open(todo_path, 'w') as f:
-            f.write("[]")
-    with open(todo_path, 'r') as f:
-        return json.load(f)
-
-def update_todo(id: int, todo: str) -> None:
-    todo_path: str = os.path.join(str(id), "todolist.json")
-    with open(todo_path, 'w') as f:
-        f.write(todo)
+from datetime import datetime
 
 class TodoCommands(Group):
-    def __init__(self):
-        super().__init__(name="todo", description="todo commands")
-
     status = [
         Choice(name="Done", value="Done"),
         Choice(name="In Progress", value="In progress"),
@@ -37,9 +20,40 @@ class TodoCommands(Group):
     }
 
     lifetime = [
-        Choice(name="Once", value="once"),
         Choice(name="Daily", value="daily"),
+        Choice(name="Once", value="once"),
     ]
+
+    def read_todo(self, id: int):
+        if not os.path.exists(str(id)):
+            return None
+        todo_path: str = os.path.join(str(id), "todolist.json")
+        if not os.path.exists(todo_path):
+            with open(todo_path, 'w') as f:
+                f.write(f"[\"{datetime.now().date()}\"]")
+        with open(todo_path, 'r') as f:
+            return json.load(f)
+
+    def update_todo(self, id: int, todo) -> None:
+        todo_path: str = os.path.join(str(id), "todolist.json")
+        with open(todo_path, 'w') as f:
+            f.write(json.dumps(todo))
+
+    def update_lifetime(self, id: int, todo):
+        date = datetime.strptime(todo[0], '%Y-%m-%d').date()
+        current_date = datetime.now().date()
+        if date == current_date:
+            return
+        for i in range(len(todo) - 1, 0, -1):
+            if todo[i][1] == "Once":
+                todo.pop(i)
+                continue
+            todo[i][2] = self.status[2].name;
+        todo[0] = str(current_date)
+        self.update_todo(id, todo)
+
+    def __init__(self):
+        super().__init__(name="todo", description="todo commands")
 
     @command(name="add", description="add task")
     @describe(name="task name")
@@ -47,24 +61,25 @@ class TodoCommands(Group):
     async def add(self, interaction: Interaction, name: str, lifetime: Optional[Choice[str]]) -> None:
         if lifetime is None:
             lifetime = self.lifetime[0] 
-        todo = read_todo(interaction.user.id)
+        todo = self.read_todo(interaction.user.id)
         if todo == None:
             await interaction.response.send_message("please use /init to initialize")
             return
         todo.append([name, lifetime.name, self.status[2].name])
-        update_todo(interaction.user.id, json.dumps(todo))
+        self.update_todo(interaction.user.id, todo)
         await interaction.response.send_message("todo - add a new task " + name)
 
     @command(name="show", description="show task")
     @choices(lifetime=lifetime)
     async def show(self, interaction: Interaction, lifetime: Optional[Choice[str]]) -> None:
-        todo = read_todo(interaction.user.id)
+        todo = self.read_todo(interaction.user.id)
         if todo == None:
             await interaction.response.send_message("please use /init to initialize")
             return
         result: str = "todo list:\n"
-        if len(todo) != 0:
-            for i in range(len(todo)):
+        if len(todo) != 1:
+            self.update_lifetime(interaction.user.id, todo)
+            for i in range(1, len(todo)):
                 if lifetime:
                     if lifetime.name == todo[i][1]:
                         result += format(f"{i} - {todo[i][0]} {self.status_char[todo[i][2]]}\n", style=Style.BulletedList)
@@ -76,23 +91,43 @@ class TodoCommands(Group):
 
     def set_status(self, user_id, todo, iden, status) -> str:
         list_len = len(todo)
-        if list_len == 0:
+        if list_len == 1:
+            return "todo - here no to task"
+        if iden.isdigit():
+            id = int(iden)
+            if list_len <= id or id < 1:
+                return f"todo - index {id} is invalid"
+
+            todo[id][2] = status.name
+            self.update_todo(user_id, todo)
+            return ""
+
+        for i in range(1, list_len):
+            if iden == todo[i][0]:
+                todo[i][2] = status.name
+                self.update_todo(user_id, todo)
+                return ""
+        return ""
+
+    def set_lifetime(self, user_id, todo, iden, lifetime) -> str:
+        list_len = len(todo)
+        if list_len == 1:
             return "todo - here is no task"
         if iden.isdigit():
             id = int(iden)
-            if list_len <= id or id < 0:
-                return f"todo - there is no id {id}"
+            if list_len <= id or id < 1:
+                return f"todo - index {id} is invalid"
 
-            todo[id][2] = status.name
-            update_todo(user_id, json.dumps(todo))
-            return f"todo - set id {id} to {status.name}"
+            todo[id][1] = lifetime.name
+            self.update_todo(user_id, todo)
+            return ""
 
-        for i in range(list_len):
+        for i in range(1, list_len):
             if iden == todo[i][0]:
-                todo[i][2] = status.name
-                update_todo(user_id, json.dumps(todo))
-                return f"todo - set name {iden} to {status.name}"
-        return f"todo - not find name {iden}"
+                todo[i][1] = lifetime.name
+                self.update_todo(user_id, todo)
+                return ""
+        return ""
 
     @command(name="set", description="set task status")
     @describe(iden="task id / name")
@@ -104,41 +139,52 @@ class TodoCommands(Group):
             await interaction.response.send_message("todo - obviously you did nothing")
             return
 
-        todo = read_todo(interaction.user.id)
+        todo = self.read_todo(interaction.user.id)
+        self.update_lifetime(interaction.user.id, todo)
+
         if status:
-            self.set_status(interaction.user.id, todo, iden, status)
+            msg = self.set_status(interaction.user.id, todo, iden, status)
+            if msg != "":
+                await interaction.response.send_message(msg)
+                return
 
         if lifetime: # TODO: implement self.set_lifetime func
-            await interaction.response.send_message(f"todo - set {iden} lifetime to {lifetime.name}")
+            msg = self.set_lifetime(interaction.user.id, todo, iden, lifetime)
+            if msg != "":
+                await interaction.response.send_message(msg)
+                return
+
         response = f"todo - set {iden} {"" if not status else status.name} {"" if not lifetime else lifetime.name}"
         await interaction.response.send_message(response)
 
     @command(name="del", description="del a task")
     @describe(iden="task id / name")
     async def delete(self, interaction: Interaction, iden: str) -> None:
-        todo = read_todo(interaction.user.id)
+        todo = self.read_todo(interaction.user.id)
         if todo == None:
             await interaction.response.send_message("please use /init to initialize")
             return
         list_len = len(todo)
-        if list_len == 0:
+        if list_len == 1:
             await interaction.response.send_message("todo - here is no task")
             return
+
+        self.update_lifetime(interaction.user.id, todo)
         if iden.isdigit():
             id = int(iden)
-            if list_len <= id or id < 0:
+            if list_len <= id or id < 1:
                 await interaction.response.send_message(f"todo - there is no id {id}")
                 return
             todo.pop(id)
-            update_todo(interaction.user.id, json.dumps(todo))
+            self.update_todo(interaction.user.id, todo)
             await interaction.response.send_message(f"todo - delete task with id {id}")
             return
 
-        for i in range(list_len):
+        for i in range(1, list_len):
             if iden == todo[i][0]:
                 todo.pop(i)
                 await interaction.response.send_message(f"todo - delete task with name {iden}")
-                update_todo(interaction.user.id, json.dumps(todo))
+                self.update_todo(interaction.user.id, todo)
                 return
         await interaction.response.send_message(f"todo - not find name {iden}")
 
