d4bd23aba4b2743a153f795ef2c997e4abffba50
LVOrac <joecheong2006@gmail.com>
2024-12-02 18:30:19 -0800

    new commands

--- src/weather_commands.py
+++ src/weather_commands.py
@@ -4,14 +4,15 @@ import json
 from discord import Interaction
 from discord.app_commands import Group, command, describe
 from dotenv import load_dotenv
-from user import load_json, user_initialized
+from user import load_json, user_initialized, update_today_is_today
 
 load_dotenv()
-TOKEN: str = str(os.getenv("WEATHER_API_KEY"))
+API_2_5: str = str(os.getenv("WEATHER_API_2_5"))
 
 day_icons = {
         "clear sky": "🔆",
         "few clouds": "🌤️",
+        "overcast clouds": "🌥️",
         "scattered clouds": "☁",
         "broken clouds": "🌥️",
         "slower rain": "🌦️",
@@ -25,59 +26,108 @@ class WeatherCommands(Group):
     def __init__(self):
         super().__init__(name="weather", description="weather commands")
 
-    def update_region(self, id: int, lat, lon) -> None:
-        path: str = os.path.join(str(id), "weather_region.json")
+    def update_region(self, interaction: Interaction, region: str, lat, lon) -> None:
+        path: str = os.path.join(str(interaction.user.id), "weather_region.json")
         with open(path, 'w') as f:
-            f.write(f"[{lat}, {lon}]")
+            f.write(f"[\"{region}\",{lat},{lon}]")
 
-    def set_default_region(self, id: int):
-        self.update_region(id, 37.3228934, -122.0322895)
+    def set_default_region(self, interaction: Interaction):
+        self.update_region(interaction, "Cupertino", 37.3228934, -122.0322895)
 
-    def load_region(self, id: int):
-        return load_json(id, "weather_region.json")
+    def load_region(self, interaction: Interaction):
+        return load_json(interaction, "weather_region.json")
 
-    @command(name="region", description="chage region")
+    def update_weather_today(self, interaction: Interaction, region):
+        response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={region[1]}&lon={region[2]}&appid={API_2_5}&units=metric")
+
+        weather_data = json.loads(response.text)
+
+        temp = weather_data["main"]["temp"]
+        temp_max = weather_data["main"]["temp_max"]
+        temp_min = weather_data["main"]["temp_min"]
+
+        description = weather_data["weather"][0]["description"]
+        day_icon = day_icons[description]
+
+        path: str = os.path.join(str(interaction.user.id), "weather_today")
+        with open(path, 'w') as f:
+            f.write(f"My Location\n{weather_data["name"]}\n{temp:.2f}℃  {day_icon} - {description}\n{temp_min:.2f}℃ - {temp_max:.2f}℃")
+
+    @command(name="region", description="change region")
     @describe(region="region name")
     async def region(self, interaction: Interaction, region: str) -> None:
-        if msg := user_initialized(interaction.user.id):
+        if msg := user_initialized(interaction):
             await interaction.response.send_message(msg)
+            return
+
+        reg = self.load_region(interaction)
+        if reg and reg[0].lower() == region.lower():
+            await interaction.response.send_message(f"weahter - {region} has been seted")
             return
 
         try:
-            response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={region}&limit={1}&appid={TOKEN}")
+            response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={region}&limit={1}&appid={API_2_5}")
             response.raise_for_status()
             cor = json.loads(response.text)[0]
-            self.update_region(interaction.user.id, cor["lat"], cor["lon"])
-            await interaction.response.send_message("weahter - success")
-        except requests.exceptions.HTTPError as e:
-            if e.response.status_code == 404:
-                await interaction.response.send_message(f"not found region {region}")
+            self.update_region(interaction, region, cor["lat"], cor["lon"])
+            reg = self.load_region(interaction)
+            self.update_weather_today(interaction, reg)
+            await interaction.response.send_message(f"weahter - {region} {cor["lat"]} {cor["lon"]}")
+        except requests.exceptions.HTTPError:
+            await interaction.response.send_message(f"not found region {region}")
 
-
-    @command(name="show", description="today's weather")
-    async def show(self, interaction: Interaction) -> None:
-        if msg := user_initialized(interaction.user.id):
+    @command(name="today", description="today's weather")
+    async def today(self, interaction: Interaction) -> None:
+        if msg := user_initialized(interaction):
             await interaction.response.send_message(msg)
             return
 
-        region = self.load_region(interaction.user.id)
+        region = self.load_region(interaction)
         if region == None:
-            self.set_default_region(interaction.user.id)
-            region = self.load_region(interaction.user.id)
+            self.set_default_region(interaction)
+            region = self.load_region(interaction)
 
         if region == None:
             await interaction.response.send_message("weather - not found region. Please use /weather region to specify")
             return
 
-        response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={region[0]}&lon={region[1]}&appid={TOKEN}")
+        path: str = os.path.join(str(interaction.user.id), "weather_today")
+        if not os.path.exists(path) or update_today_is_today(interaction):
+            try:
+                self.update_weather_today(interaction, region)
+            except requests.exceptions.HTTPError:
+                await interaction.response.send_message(f"not found region {region}")
 
-        weather_data = json.loads(response.text)
+        with open(path, 'r') as f:
+            await interaction.response.send_message(str(f.read()))
 
-        temp = weather_data["main"]["temp"] - 273.15
-        temp_max = weather_data["main"]["temp_max"] - 273.15
-        temp_min = weather_data["main"]["temp_min"] - 273.15
+    @command(name="forecast", description="forecast")
+    async def forecast(self, interaction: Interaction) -> None:
+        if msg := user_initialized(interaction):
+            await interaction.response.send_message(msg)
+            return
 
-        description = weather_data["weather"][0]["description"]
-        day_icon = day_icons[description]
+        region = self.load_region(interaction)
+        if region == None:
+            self.set_default_region(interaction)
+            region = self.load_region(interaction)
 
-        await interaction.response.send_message(f"My Location\n{weather_data["name"]}\n{temp:.2f}℃  {day_icon} - {description}\n{temp_min:.2f}℃ - {temp_max:.2f}℃")
+        if region == None:
+            await interaction.response.send_message("weather - not found region. Please use /weather region to specify")
+            return
+
+        try:
+            response = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?lat={region[1]}&lon={region[2]}&appid={API_2_5}&units=metric")
+            forecast = json.loads(response.text)["list"]
+            result = ""
+            for weather_data in forecast:
+                temp = weather_data["main"]["temp"]
+                description = weather_data["weather"][0]["description"]
+                day_icon = day_icons[description]
+                time = weather_data["dt_txt"]
+
+                result += f"[{time}]  {day_icon} {temp:.2f}℃  - {description}\n"
+            await interaction.response.send_message(result)
+        except requests.exceptions.HTTPError:
+            await interaction.response.send_message(f"not found region {region}")
+
